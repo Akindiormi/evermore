4c02d829194a915f7eded4b35010d0743fa01411
Akindiormi <141813019+Akindiormi@users.noreply.github.com>
2026-09-06 22:00:35 +0100

    refactor: source Telegram URL from app config

--- lib/services/telegram_service.dart
+++ lib/services/telegram_service.dart
@@ -1,11 +1,18 @@
 import 'package:url_launcher/url_launcher.dart';
+import '../config/app_config.dart';
 
 class TelegramService {
-  static const communityUrl = 'https://t.me/earnpalnet';
+  static const communityUrl = AppConfig.telegramUrl;
 
   static Future<bool> openCommunity() async {
     final uri = Uri.parse(communityUrl);
-    if (!await canLaunchUrl(uri)) return false;
-    return launchUrl(uri, mode: LaunchMode.externalApplication);
+    try {
+      if (await canLaunchUrl(uri)) {
+        return launchUrl(uri, mode: LaunchMode.externalApplication);
+      }
+      return launchUrl(uri, mode: LaunchMode.platformDefault);
+    } catch (_) {
+      return false;
+    }
   }
 }
